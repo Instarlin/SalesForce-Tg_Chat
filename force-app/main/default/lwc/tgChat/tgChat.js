6ad3e97c563872876ebe5d445e1eb53054534dea
instarlin <instarlin@gmail.com>
2024-09-05 01:00:14 +0300

    modified:   .forceignore 	modified:   .vscode/settings.json 	deleted:    force-app/main/default/aura/.eslintrc.json 	new file:   force-app/main/default/classes/MessageController.cls 	renamed:    force-app/main/default/classes/NewUnitTest.cls-meta.xml -> force-app/main/default/classes/MessageController.cls-meta.xml 	deleted:    force-app/main/default/classes/NewApexClass.cls 	deleted:    force-app/main/default/classes/NewApexClass.cls-meta.xml 	deleted:    force-app/main/default/classes/NewUnitTest.cls 	new file:   force-app/main/default/lwc/tgChat/__tests__/tgChat.test.js 	new file:   force-app/main/default/lwc/tgChat/tgChat.css 	new file:   force-app/main/default/lwc/tgChat/tgChat.html 	new file:   force-app/main/default/lwc/tgChat/tgChat.js 	new file:   force-app/main/default/lwc/tgChat/tgChat.js-meta.xml

---
+++ force-app/main/default/lwc/tgChat/tgChat.js
@@ -0,0 +1,85 @@
+import { LightningElement, track, wire } from 'lwc';
+import createMessage from '@salesforce/apex/MessageController.createMessage';
+import getAllMessages from '@salesforce/apex/MessageController.getAllMessages';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class MessageComponent extends LightningElement {
+    @track messageBody = '';
+    @track messages = [];
+    isRendered = false;
+
+    connectedCallback() {
+        this.loadMessages();
+    }
+
+    renderedCallback() {
+        if (!this.isRendered) {
+            this.scrollToBottom();
+            this.isRendered = true;
+        }
+    }
+
+    loadMessages() {
+        getAllMessages()
+            .then(result => {
+                this.messages = result.map(message => {
+                    return {
+                        id: message.Id,
+                        body: message.Body__c,
+                        timestamp: new Date(message.CreatedDate).toLocaleString()
+                    };
+                });
+                this.isRendered = false;
+            })
+            .catch(error => {
+                this.showToast('Error', 'Failed to load messages.', 'error');
+                console.error('Error loading messages:', error);
+            });
+    }
+
+    handleInputChange(event) {
+        this.messageBody = event.target.value;
+    }
+
+    sendMessage() {
+        if (this.messageBody.trim() === '') {
+            this.showToast('Error', 'Message body cannot be empty.', 'error');
+            return;
+        }
+
+        createMessage({ messageBody: this.messageBody, senderType: 'outcoming' })
+            .then(result => {
+                this.showToast('Success', 'Message sent successfully!', 'success');
+                this.messages.push({
+                    id: result,
+                    body: this.messageBody,
+                    timestamp: new Date().toLocaleString(),
+                    type: 'outcoming'
+                });
+                this.messageBody = '';
+                this.scrollToBottom();
+            })
+            .catch(error => {
+                this.showToast('Error', 'Failed to send the message.', 'error');
+                console.error('Error:', error);
+            });
+    }
+
+    scrollToBottom() {
+        requestAnimationFrame(() => {
+            const messageList = this.template.querySelector('.messages-list');
+            if (messageList) {
+                messageList.scrollTop = messageList.scrollHeight;
+            }
+        });
+    }
+
+    showToast(title, message, variant) {
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(evt);
+    }
+}
